6275b221f59dca79ff3c9065b37dd3383dcd9596
Natalia Teplynina <nt@mnt9.com>
2022-07-29 12:27:51 +0300

    better question display

--- pollequiz/quiz/views/question.py
+++ pollequiz/quiz/views/question.py
@@ -1,6 +1,4 @@
-# from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-# from django_filters.views import FilterView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from pollequiz.quiz.models import Quiz, Question
