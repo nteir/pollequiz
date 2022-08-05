6f1a7df9895a991b4fd84687db59c5abb6cd8c79
Natalia Teplynina <nt@mnt9.com>
2022-08-05 09:56:21 +0300

    add quiz takes (for single-choice Qs only)

--- pollequiz/run/views.py
+++ pollequiz/run/views.py
@@ -2,9 +2,10 @@ from django_filters.views import FilterView
 from django.views.generic.edit import CreateView
 from django.views.generic import FormView
 from pollequiz.run.forms import TakeForm, QuestionForm
-from pollequiz.quiz.models import Quiz, Question
+from pollequiz.quiz.models import Quiz, Question, Answer
 from pollequiz.quiz.filter import QuizFilter
-from pollequiz.run.models import QuizTake
+from pollequiz.run.models import QuizTake, QuizTakeLog
+from django.http import HttpResponseRedirect
 from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 import pollequiz.pq_objects as pq_objects
@@ -32,7 +33,7 @@ class TakeFirstPage(pq_objects.PQFormContextMixin, CreateView):
 
     def get_success_url(self):
         quiz_id = self.object.quiz.id
-        first_q_id = Question.objects.filter(quiz=quiz_id).order_by('q_number').first().id
+        first_q_id = Question.objects.filter(quiz=quiz_id).order_by('q_number', 'id').first().id
         return reverse_lazy('run:quiz_take', kwargs={'quiz_id': quiz_id, 'pk': self.object.id, 'q_id': first_q_id})
 
     def get_initial(self):
@@ -57,18 +58,39 @@ class TakeQuestion(FormView):
     form_class = QuestionForm
     template_name = 'run/question.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # quiz = Quiz.objects.get(id=self.kwargs['quiz_id'])
-        # q_list = list(Question.objects.filter(quiz=quiz.id).order_by('q_number'))
-        return context
-
     def get_form(self, form_class=QuestionForm):
         q_id = self.kwargs.get('q_id')
         if q_id:
             self.question = Question.objects.get(id=q_id)
         return form_class(**self.get_form_kwargs())
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        question = Question.objects.get(id=self.kwargs['q_id'])
+        context['question'] = question
+        return context
+
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
         return dict(kwargs, question=self.question)
+
+    def get_success_url(self):
+        quiz = Quiz.objects.get(id=self.kwargs['quiz_id'])
+        q_list = list(Question.objects.values('id').filter(quiz=quiz.id).order_by('q_number', 'id'))
+        q_list = [q['id'] for q in q_list]
+        curr_id = self.kwargs.get('q_id')
+        curr_index = q_list.index(curr_id)
+        if curr_index + 1 >= len(q_list):
+            url = reverse_lazy('home')
+        else:
+            next = q_list[curr_index + 1]
+            url = reverse_lazy('run:quiz_take', kwargs={'quiz_id': quiz.id, 'pk': self.kwargs['pk'], 'q_id': next})
+        return url
+
+    def form_valid(self, form):
+        log_entry = QuizTakeLog()
+        log_entry.take_id = QuizTake.objects.get(id=self.kwargs.get('pk'))
+        log_entry.q_id = self.question
+        log_entry.a_id = Answer.objects.get(id=form.data.get('answers'))
+        log_entry.save()
+        return HttpResponseRedirect(self.get_success_url())
