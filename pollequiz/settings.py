9e5430e63b0e486dd7781d656ad84b41689dee81
Natalia Teplynina <nt@mnt9.com>
2022-08-12 14:03:58 +0300

    edit settings for static

--- pollequiz/settings.py
+++ pollequiz/settings.py
@@ -136,13 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-if DEBUG:
-    STATICFILES_DIRS = [
-        BASE_DIR / 'static'
-    ]
-else:
-    STATIC_ROOT = BASE_DIR / 'static'
-
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATIC_URL = 'static/'
 
