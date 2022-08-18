193894299863d583a0a397c43830b211e3a0e54d
Natalia Teplynina <nt@mnt9.com>
2022-08-18 11:48:45 +0300

    fix filtering for My quizzes page

--- pollequiz/quiz/filter.py
+++ pollequiz/quiz/filter.py
@@ -3,22 +3,23 @@ from pollequiz.quiz.models import Quiz
 import pollequiz.text_constants as txt
 
 
-class QuizFilter(django_filters.FilterSet):
+class MyQuizFilter(django_filters.FilterSet):
     POLL_CHOICES = (
         (False, 'Quiz'),
         (True, 'Poll'),
     )
 
     name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
-    author = django_filters.CharFilter(field_name='author', lookup_expr='icontains')
     is_poll = django_filters.ChoiceFilter(field_name='is_poll', choices=POLL_CHOICES, label=txt.POLL_FILTER_LABEL)
 
     class Meta:
         model = Quiz
-        fields = ['name', 'author', 'is_poll']
+        fields = ['name', 'is_poll']
 
 
-class MyQuizFilter(QuizFilter):
+class QuizFilter(MyQuizFilter):
+    author = django_filters.CharFilter(field_name='author', lookup_expr='icontains')
+
     class Meta:
         model = Quiz
-        fields = ['name', 'is_poll']
+        fields = ['name', 'author', 'is_poll']
